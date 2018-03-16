0d481019e066f1271201813873b57a18739e7830
xwzcwq <295466244@qq.com>
2018-03-16 21:52:17 +0800

    Write a MVC file

---
+++ www/handlers.py
@@ -0,0 +1,13 @@
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
